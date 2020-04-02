fa2331b0065ffd53f53de56ce006c7527de83559
Michal.Klosiewicz <michal.klosiewicz@student.wat.edu.pl>
2020-04-02 11:08:16 +0200

    Zadanie 2.2.2

--- Zadanie 2.2.1b/src/main.cpp
+++ Zadanie 2.2.1b/src/main.cpp
@@ -1,15 +1,16 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, LOW);
-  }
+#define LED_RED 10
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(500);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(500);
+void setup()
+{
+  pinMode(LED_RED, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(LED_RED, HIGH);
+  delay(500); // Wait for 500 millisecond(s)
+  digitalWrite(LED_RED, LOW);
+  delay(500); // Wait for 500 millisecond(s)
 }
