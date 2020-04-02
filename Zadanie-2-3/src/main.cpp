27785f4d9a4835ed5860f2e1e416ba20f59367c1
Korneliusz Dudus <korneliusz.dudus@student.wat.edu.pl>
2020-04-02 09:57:25 +0200

    Zakończono zadanie 2.2.3

--- Zadanie-2-3/src/main.cpp
+++ Zadanie-2-3/src/main.cpp
@@ -5,18 +5,42 @@ uint8_t ledDelay = 65;
 uint8_t direction = 1;
 uint8_t currentLED = 0;
 unsigned long changeTime;
-void changeLED();
+#define LED_RED 10
+#define M_KROPKA_T 200
+#define M_KRESKA_T 3 * M_KROPKA_T
+#define M_PAUZA_T 3 * M_KROPKA_T
+#define M_SPACJA_T 7 * M_KROPKA_T
+
+
 
 void setup() {
-  pinMode(10,OUTPUT);
+  pinMode(LED_RED,OUTPUT);
   
 }
 
 void loop() {
-  digitalWrite(10,HIGH);
-  delay(500);
-  digitalWrite(10, LOW);
-  delay(500);
+  for(int i = 0; i<3;i++)
+  {
+    digitalWrite(LED_RED,HIGH);
+    delay(M_KROPKA_T);
+    digitalWrite(LED_RED,LOW);
+    delay(M_PAUZA_T);
+  }
+  for(int i = 0; i< 3;i++)
+  {
+    digitalWrite(LED_RED,HIGH);
+    delay(M_KRESKA_T);
+    digitalWrite(LED_RED,LOW);
+    delay(M_PAUZA_T);
+  }
+  for(int i =0; i<3; i++)
+  {
+    digitalWrite(LED_RED,HIGH);
+    delay(M_KROPKA_T);
+    digitalWrite(LED_RED,LOW);
+    delay(M_PAUZA_T);
+  }
+  delay(M_SPACJA_T);
 
 }
 
