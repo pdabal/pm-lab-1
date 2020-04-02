0335b8417df92785f17f57715fbe230f905ab77d
Kordian Kucharek <kordian.kucharek@student.wat.edu.pl>
2020-04-02 11:19:08 +0200

    Zakończono zadanie 2.2.3

--- Zadanie 2-2/src/main.cpp
+++ Zadanie 2-2/src/main.cpp
@@ -2,15 +2,37 @@
 
 #define LED_RED 10
 
+// Sposób kodowania
+#define M_KROPKA_T 200
+#define M_KRESKA_T 3 * M_KROPKA_T
+#define M_PAUZA_T 3 * M_KROPKA_T
+#define M_SPACJA_T 7 * M_KROPKA_T
+
 void setup() {
-  // put your setup code here, to run once:
   pinMode(LED_RED, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  for(int i = 0; i < 3; i++) //Wysyłanie znaku 'S'
+  {
     digitalWrite(LED_RED, HIGH);
-    delay(500);
+    delay(M_KROPKA_T);
     digitalWrite(LED_RED, LOW);
-    delay(500);
+    delay(M_PAUZA_T);
+  }
+  for(int i = 0;i < 3; i++) //Wysyłanie znaku 'O'
+  {
+   digitalWrite(LED_RED, HIGH);
+   delay(M_KRESKA_T);
+   digitalWrite(LED_RED, LOW);
+   delay(M_PAUZA_T);
+  }
+  for(int i = 0; i < 3; i++) //Wysyłanie znaku 'S'
+  {
+    digitalWrite(LED_RED, HIGH);
+    delay(M_KROPKA_T);
+    digitalWrite(LED_RED, LOW);
+    delay(M_PAUZA_T);
+  }
+  delay(M_SPACJA_T);
 }
