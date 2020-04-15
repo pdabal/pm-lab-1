2fbdbae9f6e2f4d619ed45987ddabbd91d9e65a2
Jakub Wanat <jakub.wanat@student.wat.edu.pl>
2020-04-15 11:25:15 +0200

    Zakończono zadanie 2.2.3

--- Zadanie-2-3/src/main.cpp
+++ Zadanie-2-3/src/main.cpp
@@ -1,28 +1,37 @@
 #include <Arduino.h>
 
-uint8_t ledPin[] = {4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
-uint8_t ledDelay = 65;
-uint8_t direction = 1;
-uint8_t currentLED = 0;
-unsigned long changeTime;
-
 #define LED_RED 10
-void changeLED();
-
+#define M_KROPKA_T 200
+#define M_KRESKA_T 3 * M_KROPKA_T
+#define M_PAUZA_T 3 * M_KROPKA_T
+#define M_SPACJA_T 7 * M_KROPKA_T
 
 
 void setup() {
-  pinMode(LED_RED, OUTPUT);
-  
-  
+pinMode(LED_RED, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(LED_RED, HIGH);
-  delay(500);
-  digitalWrite(LED_RED, LOW);
-   delay(500);
+  for( int i= 0; i < 3; i++) // Wysłanie znkau 'S'
+{
+digitalWrite(LED_RED, HIGH);
+delay(M_KROPKA_T);
+digitalWrite(LED_RED, LOW);
+delay(M_PAUZA_T);
 }
-
-
-
+for( int i= 0; i < 3; i++) // Wysłanie znkau '0'
+{
+digitalWrite(LED_RED, HIGH);
+delay(M_KROPKA_T);
+digitalWrite(LED_RED, LOW);
+delay(M_PAUZA_T);
+}
+for( int i= 0; i < 3; i++) // Wysłanie znkau 'S'
+{
+digitalWrite(LED_RED, HIGH);
+delay(M_KROPKA_T);
+digitalWrite(LED_RED, LOW);
+delay(M_PAUZA_T);
+}
+delay(M_SPACJA_T);
+}
