19404736c26d3b52ce6d58eccf34cc60f9908f9b
Wojciech Szymański <wojciech.szymanski@student.wat.edu.pl>
2020-04-15 12:00:48 +0200

    wykonano zadanie 2.2.3

--- Zadanie-2-3/src/main.cpp
+++ Zadanie-2-3/src/main.cpp
@@ -1,7 +1,14 @@
+
+
 #include <Arduino.h>
 
 #define LED_RED 10
 
+//Sposob kodowania
+#define M_KROPKA_T 200
+#define M_KRESKA_T 3 * M_KROPKA_T
+#define M_PAUZA_T 3 * M_KROPKA_T
+#define M_SPACJA_T 7 * M_KROPKA_T
 
 
 void changeLED();
@@ -14,13 +21,36 @@ void setup() {
 
 void loop() {
  
+  for (int i = 0; i < 3; i++) //Wysylanie znaku 'S'
 
-  digitalWrite(LED_RED, HIGH);
-  delay(500);
-  digitalWrite(LED_RED, LOW);
-  delay(500);
+  {
+    digitalWrite(LED_RED, HIGH);
+    delay(M_KROPKA_T);
+    digitalWrite(LED_RED, LOW);
+    delay(M_PAUZA_T);
+  }
+
+  for (int i = 0; i < 3; i++)  //wysylanie znaku '0'
+  {
+    digitalWrite(LED_RED, HIGH);
+    delay(M_KRESKA_T);
+    digitalWrite(LED_RED, LOW);
+    delay(M_PAUZA_T);
+
+  }
+  for (int i = 0; i < 3; i++)  //wysylanie znaku 'S'
+  {
+    digitalWrite(LED_RED, HIGH);
+    delay(M_KROPKA_T);
+    digitalWrite(LED_RED, LOW);
+    delay(M_PAUZA_T);
+
+  }
+
+  delay(M_SPACJA_T);
+  
+
 }
-
 
 
 
