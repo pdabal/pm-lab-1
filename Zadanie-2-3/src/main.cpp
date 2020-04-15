ec915d4315dcc9fe1c3174ee1f300226fa6faa90
Karol Szeliga <karol.szeliga@student.wat.edu.pl>
2020-04-15 11:11:33 +0200

    Wykonano zadanie 2.2.2

--- Zadanie-2-3/src/main.cpp
+++ Zadanie-2-3/src/main.cpp
@@ -1,13 +1,14 @@
 #include <Arduino.h>
 
-void setup() {
+#define LED_RED 10
 
-pinMode(LED_BUILTIN, OUTPUT);
-digitalWrite(LED_BUILTIN,LOW);
+void setup() {
+pinMode(LED_RED, OUTPUT);
 }
 
 void loop() {
- 
- digitalWrite(LED_BUILTIN,HIGH);
- digitalWrite(LED_BUILTIN,LOW);
+ digitalWrite(LED_RED,HIGH);
+ delay(500);
+ digitalWrite(LED_RED,LOW);
+ delay(500);
 }
